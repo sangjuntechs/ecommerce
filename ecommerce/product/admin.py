30885ef718f6f5c57e81c2ff2fe2adf28c2d3cbe
sangjun <juntech0421@github.com>
2020-05-30 16:32:29 +0900

    modify timezone

--- ecommerce/product/admin.py
+++ ecommerce/product/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','price')
+    list_display = ('name','price','register_date')
 
 admin.site.register(Product, ProductAdmin)
